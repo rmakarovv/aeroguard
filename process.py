f81343e7b126f8ecd16697c1f00b6f21894955f7
Shinypuff <mtmnt1@mail.ru>
2024-10-22 10:44:32 +0300

    save functionality added

--- process.py
+++ process.py
@@ -12,6 +12,21 @@ from PIL import Image
 from torchvision import transforms as tf
 
 
+def get_boxes(boxes_list):
+    
+    boxes = []
+
+    for i, row in enumerate(boxes_list):
+        x_min, y_min, x_max, y_max = row
+
+        width = x_max - x_min
+        height = y_max - y_min
+
+        boxes.append([y_min.item(), x_min.item(), height.item(), width.item()])
+
+    return boxes
+
+
 def main():
     
     with open('config.yaml', 'rb') as f:
@@ -19,9 +34,11 @@ def main():
     
     detect = YOLO(cfg['detection'])
     clf = YOLO(cfg['classification'])
-    path = cfg['data']
+    path = cfg['data'] + '/'
 
     pic_name = np.random.choice(os.listdir(path)) # the picture is chosen randomly
+
+    print(pic_name)
 
     img_path = path + pic_name
     detected = detect.predict(source=img_path, save=False, save_txt=False)
@@ -36,19 +53,16 @@ def main():
         height = y_max - y_min
 
         boxes.append([y_min.item(), x_min.item(), height.item(), width.item()])
-
-    cropped_pics = []
     
     for i, box in enumerate(boxes):
-        cropped_pics.append(tf.functional.crop(Image.open(img_path), *box))
-
-    for cropped in cropped_pics:
+        cropped = tf.functional.crop(Image.open(img_path), *box)
         classified = clf.predict(cropped, save=False, save_txt=False)
         
-        tmp = classified[0].plot()
-        plt.imshow(Image.fromarray(tmp))
-        plt.show()
+        img_array = classified[0].plot()
 
+        if cfg['savepath']:
+            img_to_save = Image.fromarray(img_array)
+            img_to_save.save(cfg['savepath'] + '/' + f'{i}_{pic_name}')
 
 if __name__ == '__main__':
     main()
