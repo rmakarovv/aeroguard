496742e1d2078fa0a01b70c6b8295c6c9c53b170
amina <noteisenheim@gmail.com>
2024-10-23 19:44:12 +0300

    process all the images in folder, save bounding boxes

--- scripts/process.py
+++ scripts/process.py
@@ -33,6 +33,8 @@ def main():
     parser.add_argument('-d', '--data', type=str, default=cfg['data'], dest='data')
     parser.add_argument('-cw', '--classification', type=str, default=cfg['classification'], dest='classification')
     parser.add_argument('-dw', '--detection', type=str, default=cfg['detection'], dest='detection')
+    parser.add_argument('-sb', '--save_bboxes', action='store_true', dest='save_bboxes')
+    parser.set_defaults(save_bboxes=False)
 
     args = parser.parse_args()
     
@@ -40,36 +42,39 @@ def main():
     clf = YOLO(args.classification)
     path = args.data + '/'
 
-    pic_name = np.random.choice(os.listdir(path)) # the picture is chosen randomly
+    if args.savepath and args.save_bboxes:
+        os.makedirs(args.savepath + "_bboxes/", exist_ok=True)
 
-    print(pic_name)
+    pic_names = os.listdir(path)
+    for pic_name in pic_names:
+        print(f'Processing {pic_name}')
 
-    img_path = path + pic_name
-    detected = detect.predict(source=img_path, save=False, save_txt=False)
+        img_path = path + pic_name
+        detected = detect.predict(source=img_path, save=False, save_txt=False)
 
-    box = detected[0].boxes.xyxy.int()
+        box = detected[0].boxes.xyxy.int()
 
-    boxes = []
-    for i, row in enumerate(box):
-        x_min, y_min, x_max, y_max = row
+        boxes = []
+        for i, row in enumerate(box):
+            x_min, y_min, x_max, y_max = row
 
-        width = x_max - x_min
-        height = y_max - y_min
+            width = x_max - x_min
+            height = y_max - y_min
 
-        boxes.append([y_min.item(), x_min.item(), height.item(), width.item()])
-    
-    if args.savepath:
-        np.save(args.savepath + '/boxes.npy', np.array(boxes))
-    
-    for i, box in enumerate(boxes):
-        cropped = tf.functional.crop(Image.open(img_path), *box)
-        classified = clf.predict(cropped, save=False, save_txt=False)
+            boxes.append([y_min.item(), x_min.item(), height.item(), width.item()])
         
-        img_array = classified[0].plot()
-
         if args.savepath:
-            img_to_save = Image.fromarray(img_array)
-            img_to_save.save(args.savepath + '/' + f'{i}_{pic_name}')
+            np.save(args.savepath + '_bboxes/' + pic_name.split('.')[0] + '.npy', np.array(boxes))
+        
+        for i, box in enumerate(boxes):
+            cropped = tf.functional.crop(Image.open(img_path), *box)
+            classified = clf.predict(cropped, save=False, save_txt=False)
+            
+            img_array = classified[0].plot()
+
+            if args.savepath:
+                img_to_save = Image.fromarray(img_array)
+                img_to_save.save(args.savepath + '/' + f'{i}_{pic_name}')
 
 if __name__ == '__main__':
     main()
