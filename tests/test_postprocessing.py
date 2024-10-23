4ee2de72926efefa6f3ce01e96de47e6b77f83cf
amina <noteisenheim@gmail.com>
2024-10-23 19:51:03 +0300

    add tests for postprocessing

--- tests/test_postprocessing.py
+++ tests/test_postprocessing.py
@@ -1,11 +1,25 @@
+import os
+import shutil
 import unittest
 
 class TestPostprocessing(unittest.TestCase):
-    def test_postprocessing(self):
-        self.assertEqual(1, 1)
+    def setUp(self):
+        self.input_dir = "/app/tests/images_out"
+        self.output_dir = "/app/tests/output_tmp"
+        os.mkdir(self.output_dir)
+        os.system("/app/scripts/postprocess /app/tests/images_out /app/tests/output_tmp/result.pdf")
+    
+    def test_report_is_generated(self):
+        self.assertTrue(os.path.exists(os.path.join(self.output_dir, "result.pdf")))
+
+    def test_report_not_empty(self):
+        self.assertTrue(os.path.getsize(os.path.join(self.output_dir, "result.pdf")) > 0)
+
+    def tearDown(self):
+        shutil.rmtree(self.output_dir)
 
 if __name__ == '__main__':
-    log_file = 'log_postprocessing_test.txt'
+    log_file = '/app/logs/log_postprocessing_test.txt'
     with open(log_file, "w") as f:
         runner = unittest.TextTestRunner(f, verbosity=2)
         unittest.main(testRunner=runner)
